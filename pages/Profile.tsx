c6845cf415f0165e24d3700830bd760d2f16253b
ziale <ziale.code@gmail.com>
2020-12-29 18:59:15 +0100

    Refactor

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 import "./Profile.css";
 
 const profile = props => {
+  const profileInfoData = props.person.data.map(next => {
+    return (
+      <div className="profile-info-data">
+        <div className="profile-info-data__number">{next.value}</div>
+        <div className="profile-info-data__name">{next.display}</div>
+      </div>
+    );
+  });
+
   return (
     <div className="page">
       <div className="profile">
@@ -19,32 +28,7 @@ const profile = props => {
 
             <h1 className="profile-header__text">{props.person.name}</h1>
           </div>
-          <div className="profile-info">
-            <div className="profile-info-data">
-              <div className="profile-info-data__number">
-                {props.person.data[0].value}
-              </div>
-              <div className="profile-info-data__name">
-                {props.person.data[0].display}
-              </div>
-            </div>
-            <div className="profile-info-data">
-              <div className="profile-info-data__number">
-                {props.person.data[1].value}
-              </div>
-              <div className="profile-info-data__name">
-                {props.person.data[1].display}
-              </div>
-            </div>
-            <div className="profile-info-data">
-              <div className="profile-info-data__number">
-                {props.person.data[2].value}
-              </div>
-              <div className="profile-info-data__name">
-                {props.person.data[2].display}
-              </div>
-            </div>
-          </div>
+          <div className="profile-info">{profileInfoData}</div>
         </div>
       </div>
     </div>
